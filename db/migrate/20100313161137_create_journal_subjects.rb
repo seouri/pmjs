00af63ba126469c085f6f8e67e100a801d812872
Joon Lee <seouri@gmail.com>
2010-03-26 21:29:20 -0400

    added full articles counts to JournalSubject

--- db/migrate/20100313161137_create_journal_subjects.rb
+++ db/migrate/20100313161137_create_journal_subjects.rb
@@ -7,6 +7,11 @@ class CreateJournalSubjects < ActiveRecord::Migration
       t.integer :direct_articles_count,     :default => 0, :limit => 2
       t.integer :descendant_articles_count, :default => 0, :limit => 3
       t.integer :articles_count,            :default => 0, :limit => 3
+      t.integer :start_year,                :default => 0, :limit => 2
+      t.integer :end_year,                  :default => 0, :limit => 2
+      t.text    :direct_articles_counts
+      t.text    :descendant_articles_counts
+      t.text    :articles_counts
     end
     add_index :journal_subjects, [:journal_id, :year, :direct_articles_count, :articles_count], :name => 'index_journal_subjects_on_journal_id_year_direct_articles_count'
     add_index :journal_subjects, [:subject_id, :year, :direct_articles_count, :articles_count], :name => 'index_journal_subjects_on_subject_id_year_direct_articles_count'
